739e25fe229a75ab0aeae41b59824f5fe3a3fd05
Artempoo <artempoo@gmail.com>
2021-09-13 17:41:34 +0700

    save

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -4,6 +4,17 @@
 
   <button class="button">button</button>
   <button class="button-reds">button</button>
+
+  <h1 v-text='`Calibri H${1} Regular`'/>
+  <h2 v-text='`Calibri H${2} Regular`'/>
+  <h3 v-text='`Calibri H${3} Regular`'/>
+  <h4 v-text='`Calibri H${4} Regular`'/>
+  <h5 v-text='`Calibri H${5} Regular`'/>
+  <h6 v-text='`Calibri H${6} Regular`'/>
+
+}
+
+
   <ProductTile/>
 </template>
 
